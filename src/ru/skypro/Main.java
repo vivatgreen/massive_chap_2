560baa2c8bd282d54803bfd2e7cb4d096fd99549
vivatgreen <grindsasha@bk.ru>
2022-03-21 02:43:32 +0300

    Массивы

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,21 +1,8 @@
 package ru.skypro;
 import java.util.Arrays;
 public class Main {
-
-    public static void main(String[] args) {
-        // Задача 4
-        char[]reverseFullName={'n','a','v','I',' ','v','o','n','a','v','I'};
-        int length = 0;
-        for (int ignored : reverseFullName) length++;
-        for (int i = length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-        }
-
-        System.out.println();
-
-        int[] arr = generateRandomArray();
-    }
-    private static int[] generateRandomArray() {
+    int[] arr = generateRandomArray();
+    public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
@@ -23,38 +10,55 @@ public class Main {
         }
         System.out.println(Arrays.toString(arr));
 
-        //
-        int sum = 0;
-        sum = Arrays.stream(arr).sum();
-        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
-
-        //Min
-
-        int min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (min > arr[i]) {
-                min = arr[i];
-            }
-        }
-        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
-
-        //Max
-
-        int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
-            }
-        }
-        System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
-
-        // Avarage
-
-        double avarage = 0;
-        for (int i = 0; i < arr.length; i++) {
-            avarage += arr[i] / arr.length;
-        }
-        System.out.println("Средняя сумма трат за месяц составила " + avarage + " рублей");
         return arr;
     }
-}
+
+        public static void main (String[] args) {
+
+        java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000, 200_000);
+            }
+            System.out.println(Arrays.toString(arr));
+
+            int sum = 0;
+            sum = Arrays.stream(arr).sum();
+            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+            //Min + Max
+
+            int min = arr[0];
+            int max = arr[0];
+            for (int i = 0; i < arr.length; i++)
+                if (min > arr[i]) {
+                    min = arr[i];
+                } else if (max < arr[i]) {
+                    max = arr[i];
+                }
+            System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+            System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
+
+            // Avarage
+
+            double avarage = 0;
+            for (int i = 0; i < arr.length; i++) {
+                avarage += arr[i] / arr.length;
+            }
+            System.out.println("Средняя сумма трат за месяц составила " + avarage + " рублей");
+
+            //Задача 4
+            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            int length = 0;
+            for (int i = reverseFullName.length - 1; i >= 0; i--) {
+                System.out.print(reverseFullName[i]);
+            }
+
+            System.out.println();
+        }
+    }
+
+
+
+
+
